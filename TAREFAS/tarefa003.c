58abb7bb5dffe9431cece16b2c183406e37ee58b
Lucas <lucasminneh270508@gmail.com>
2026-08-10 13:44:54 -0300

    aula 10/08

---
+++ TAREFAS/tarefa003.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+    float  am, tm, ta, va;
+    int t, cont;
+    cont = 0;
+    printf("Digite a sua Aplicacao inicial: ");
+    scanf("%f", &ta);
+    printf("Digite o seu Aporte Mensal: ");
+    scanf("%f", &am);
+    printf("Digite a taxa mensal: ");
+    scanf("%f", &tm);
+    printf("Digite a quantidade de Meses: \n");
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        printf("Mes [%.2d]: Saldo - [%15.2f].\n", i, ta);
+        ta = (ta + (ta * (tm / 100)) + am);
+    }
+    printf("Mes [%.2d]: Saldo - [%15.2f].\n", t, ta);
+
+    printf("Digite o valor que voce ira retirar na sua aposentadoria: ");
+    scanf("%f", &va);
+    while (ta > 0 && cont < 1200) {
+        printf("Mes Aposentado [%.2d]: Saldo - [%15.2f].\n", cont, ta);
+        ta = ta + ( ta * (tm / 100)) - va ;
+        cont++ ;
+    }
+}
