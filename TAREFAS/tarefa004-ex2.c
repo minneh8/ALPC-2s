3e4f10cfabde74660e07eb14a50beefc3ecd09fc
Lucas <lucasminneh270508@gmail.com>
2026-08-13 20:06:10 -0300

    metade do segundo exercicio da terafa004 feito

--- TAREFAS/tarefa004-ex2.c
+++ TAREFAS/tarefa004-ex2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main () {
-    
-
+    int idade, media, novo, velho, um, dois, tres, quatro, cinco, n, somaidade; 
+    printf("Digite quantos aluno voce quer coletar a informacao: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Quantos anos o aluno %d tem: ", i + 1);
+        scanf("%d", &idade);
+        somaidade = somaidade + idade;
+    }
 }
