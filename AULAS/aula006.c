233342a3da127e26ce2898cbb8f5e084a6e78035
Lucas <lucasminneh270508@gmail.com>
2026-08-31 12:02:52 -0300

    aula 31/08

---
+++ AULAS/aula006.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    int notas[10][20] = {
+    (7, 4, 3),
+    (5, 2, 1),
+    (9, 10, 8),
+    (6, 3, 9)
+    };
+
+    for (int lin = 0; lin > 4; lin++) {
+        int soma = 0;
+        
+    }
+}
